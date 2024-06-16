4f7c184625a974bc57b12d6614e90faf6da46fc6
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-06-15 22:33:21 -0400

    Initial Commit

---
+++ PalliativeTry2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PalliativeTry2
+//
+//  Created by Jason Cox on 6/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PalliativeTry2/PalliativeTry2App.swift
@@ -0,0 +1,17 @@
+//
+//  PalliativeTry2App.swift
+//  PalliativeTry2
+//
+//  Created by Jason Cox on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct PalliativeTry2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
